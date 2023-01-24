e9f1ea8f81db8e212f1c2f115e31d89c9c60a54e
benshabbat <benshabbat27@gmail.com>
2023-01-24 23:05:36 +0200

    backend for booking

---
+++ routes/hotels.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  countByCity,
+  countByType,
+  getHotelRooms
+} from "../controllers/hotel.js";
+import { verifyAdmin} from "../utils/verifyToken.js";
+const router = express.Router();
+
+//CREATE
+router.post("/",verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id",verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getHotels);
+router.get("/countbycity", countByCity);
+router.get("/countbytype", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router;
